48080fce30c0d0ff65a7294ba96fdf5394dcec40
Lord-Bubbles <justinhanyueli@gmail.com>
2024-05-26 00:05:55 -0500

    change User model to always include Account navigation property

--- VueApp1.Server/Helpers/AppDbContext.cs
+++ VueApp1.Server/Helpers/AppDbContext.cs
@@ -5,19 +5,21 @@ namespace VueApp1.Server.Helpers;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+  {
+  }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.UseSerialColumns();
-    }
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.UseSerialColumns();
 
-    public DbSet<User> Users { get; set; }
+    modelBuilder.Entity<User>().Navigation(u => u.Account).AutoInclude();
+  }
 
-    public DbSet<Account> Accounts { get; set; }
+  public DbSet<User> Users { get; set; }
 
-    public DbSet<Performance> Performances { get; set; }
+  public DbSet<Account> Accounts { get; set; }
+
+  public DbSet<Performance> Performances { get; set; }
 
 }
