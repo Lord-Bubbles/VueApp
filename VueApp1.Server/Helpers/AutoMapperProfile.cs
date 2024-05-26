1299f101153588000853cf69cd0f29a2c734488d
Lord-Bubbles <justinhanyueli@gmail.com>
2024-05-26 00:05:18 -0500

    add more mappings and condition checks

--- VueApp1.Server/Helpers/AutoMapperProfile.cs
+++ VueApp1.Server/Helpers/AutoMapperProfile.cs
@@ -5,22 +5,23 @@ using VueApp1.Server.Models;
 
 public class AutoMapperProfile : Profile
 {
-    public AutoMapperProfile()
-    {
-        CreateMap<User, AuthenticateResponse>();
+  public AutoMapperProfile()
+  {
+    CreateMap<User, AuthenticateResponse>();
 
-        CreateMap<UpdateRequest, User>()
-            .ForAllMembers(x => x.Condition((src, dest, prop) =>
-            {
-                // Ignore null fields
-                if (prop == null) { return false; }
-                // Ignore empty or null string fields
-                if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) { return false; }
-                // Ignore the ID field
-                if (prop.GetType() == typeof(int) && (int)prop <= 0) { return false; }
-                return true;
-            }));
+    CreateMap<User, UserView>();
 
-        CreateMap<User, UserView>();
-    }
+    CreateMap<UpdateRequest, User>()
+        .ForAllMembers(x => x.Condition((src, dest, prop) =>
+        {
+          // Ignore null fields
+          if (prop == null) { return false; }
+          // Ignore empty or null string fields
+          if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) { return false; }
+          // Ignore non positive integer fields
+          if (prop.GetType() == typeof(int) && (int)prop <= 0) { return false; }
+          return true;
+        }));
+
+  }
 }
