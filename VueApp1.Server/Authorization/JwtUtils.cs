914a5e6b8764697aa89d82e521c3b2b6b26a4b35
Lord-Bubbles <justinhanyueli@gmail.com>
2024-05-26 00:04:20 -0500

    change secret key generation to using RandomNumberGenerator

--- VueApp1.Server/Authorization/JwtUtils.cs
+++ VueApp1.Server/Authorization/JwtUtils.cs
@@ -9,54 +9,55 @@ namespace VueApp1.Server.Authorization;
 
 public interface IJwtUtils
 {
-    public string GenerateToken(User user);
-    public int ValidateToken(string token);
+  public string GenerateToken(User user);
+  public int ValidateToken(string token);
 }
 
 public class JwtUtils : IJwtUtils
 {
-    private readonly Aes aes = Aes.Create();
-    public string GenerateToken(User user)
+  private static readonly byte[] secret = RandomNumberGenerator.GetBytes(16);
+
+  public string GenerateToken(User user)
+  {
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var tokenDescriptor = new SecurityTokenDescriptor
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity([new Claim("id", user.ID.ToString())]),
-            Expires = DateTime.UtcNow.AddDays(1),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(aes.Key), SecurityAlgorithms.HmacSha256Signature),
-            IssuedAt = DateTime.UtcNow
-        };
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
+      Subject = new ClaimsIdentity([new Claim("id", user.ID.ToString())]),
+      Expires = DateTime.UtcNow.AddDays(1),
+      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha512Signature),
+      IssuedAt = DateTime.UtcNow
+    };
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
+  }
+
+  public int ValidateToken(string token)
+  {
+    if (token == null)
+    {
+      return -1;
     }
 
-    public int ValidateToken(string token)
+    var tokenHandler = new JwtSecurityTokenHandler();
+    try
     {
-        if (token == null)
-        {
-            return -1;
-        }
+      tokenHandler.ValidateToken(token, new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(secret),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero
+      }, out SecurityToken validatedToken);
 
-        var tokenHandler = new JwtSecurityTokenHandler();
-        try
-        {
-            tokenHandler.ValidateToken(token, new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(aes.Key),
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                ClockSkew = TimeSpan.Zero
-            }, out SecurityToken validatedToken);
+      var jwtToken = (JwtSecurityToken)validatedToken;
+      var userID = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
 
-            var jwtToken = (JwtSecurityToken)validatedToken;
-            var userID = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
-
-            return userID;
-        }
-        catch
-        {
-            return -1;
-        }
+      return userID;
     }
+    catch
+    {
+      return -1;
+    }
+  }
 }
