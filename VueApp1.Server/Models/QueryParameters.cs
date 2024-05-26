dbf5551b23c79b58002eff0bd4271a7927c93774
Lord-Bubbles <justinhanyueli@gmail.com>
2024-05-25 23:59:12 -0500

    add required attribute to fields

--- VueApp1.Server/Models/QueryParameters.cs
+++ VueApp1.Server/Models/QueryParameters.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VueApp1.Server.Models;
@@ -5,9 +6,9 @@ namespace VueApp1.Server.Models;
 public class QueryParameters
 {
 
-    [FromQuery(Name = "page")]
-    public int Page { get; set; }
+  [FromQuery(Name = "page"), Required]
+  public int Page { get; set; }
 
-    [FromQuery(Name = "limit")]
-    public int Limit { get; set; }
+  [FromQuery(Name = "limit"), Required]
+  public int Limit { get; set; }
 }
