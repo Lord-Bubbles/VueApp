fd767969091d3806a89d604456d20c1d7998c973
Lord-Bubbles <justinhanyueli@gmail.com>
2024-05-26 00:03:39 -0500

    change cast from User to UserView

--- VueApp1.Server/Authorization/AuthorizeAttribute.cs
+++ VueApp1.Server/Authorization/AuthorizeAttribute.cs
@@ -7,15 +7,15 @@ using VueApp1.Server.Models;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
-    public void OnAuthorization(AuthorizationFilterContext context)
-    {
-        // skip authorization if action is decorated with [AllowAnonymous] attribute
-        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
-        if (allowAnonymous) { return; }
+  public void OnAuthorization(AuthorizationFilterContext context)
+  {
+    // skip authorization if action is decorated with [AllowAnonymous] attribute
+    var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+    if (allowAnonymous) { return; }
 
-        // authorization
-        var user = (User)context.HttpContext.Items["User"];
-        if (user == null)
-            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
-    }
+    // authorization
+    var user = (UserView)context.HttpContext.Items["User"];
+    if (user == null)
+      context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+  }
 }
