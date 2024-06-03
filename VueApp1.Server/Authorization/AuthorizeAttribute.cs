9f111e724722803a037be5c1f1769559533fdeb2
Lord-Bubbles <justinhanyueli@gmail.com>
2024-06-03 00:00:42 -0500

    remove allow anonymous check

--- VueApp1.Server/Authorization/AuthorizeAttribute.cs
+++ VueApp1.Server/Authorization/AuthorizeAttribute.cs
@@ -2,19 +2,15 @@ namespace VueApp1.Server.Authorization;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using VueApp1.Server.Models;
+using VueApp1.Server.Models.Entities;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
   public void OnAuthorization(AuthorizationFilterContext context)
   {
-    // skip authorization if action is decorated with [AllowAnonymous] attribute
-    var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
-    if (allowAnonymous) { return; }
-
     // authorization
-    var user = (UserView)context.HttpContext.Items["User"];
+    var user = (User)context.HttpContext.Items["User"];
     if (user == null)
       context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
   }
