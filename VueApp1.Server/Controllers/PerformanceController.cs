9f66471ea7d33c6e8b8d84c152a40f1086d92784
Lord-Bubbles <justinhanyueli@gmail.com>
2024-06-02 23:55:20 -0500

    restructure code

--- VueApp1.Server/Controllers/PerformanceController.cs
+++ VueApp1.Server/Controllers/PerformanceController.cs
@@ -1,7 +1,7 @@
 namespace VueApp1.Server.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
-using VueApp1.Server.Models;
+using VueApp1.Server.Models.Entities;
 using VueApp1.Server.Services;
 using VueApp1.Server.Authorization;
 
@@ -9,14 +9,9 @@ using VueApp1.Server.Authorization;
 [ApiController]
 [Authorize]
 [Route("api/[controller]")]
-public class PerformanceController : ControllerBase
+public class PerformanceController(IPerformanceRepository repository) : ControllerBase
 {
-  private readonly IPerformanceRepository repository;
-
-  public PerformanceController(IPerformanceRepository repository)
-  {
-    this.repository = repository;
-  }
+  private readonly IPerformanceRepository repository = repository;
 
   [HttpGet]
   public IActionResult GetPerformances([FromQuery] PerformanceParameters query)
@@ -35,8 +30,8 @@ public class PerformanceController : ControllerBase
   [HttpPost]
   public async Task<IActionResult> CreatePerformance([FromBody] Performance performance)
   {
-    var perf = await repository.CreateAsync(performance);
-    return CreatedAtAction(nameof(GetPerformance), new { id = perf.ID }, perf);
+    await repository.CreateAsync(performance);
+    return Ok("Performance review successfully created!");
   }
 
   [HttpDelete("{id}")]
--- VueApp1.Server/Controllers/UserController.cs
+++ VueApp1.Server/Controllers/UserController.cs
@@ -1,62 +1,53 @@
 namespace VueApp1.Server.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
-using VueApp1.Server.Models;
+using VueApp1.Server.Models.Entities;
 using VueApp1.Server.Services;
 using VueApp1.Server.Authorization;
-using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 
 [ApiController]
 [Authorize]
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UserController(IUserRepository repository, IMapper mapper) : ControllerBase
 {
-  private readonly IUserRepository repository;
+    private readonly IUserRepository repository = repository;
 
-  public UserController(IUserRepository repository)
-  {
-    this.repository = repository;
-  }
+    private readonly IMapper mapper = mapper;
 
-  [HttpPost("authenticate"), AllowAnonymous]
-  public async Task<IActionResult> Authenticate([FromHeader(Name = "Authorization"), Required] string authorization)
-  {
-    var user = await repository.Login(authorization);
-    return Ok(user);
-  }
+    [HttpGet]
+    public IActionResult GetUsers([FromQuery] UserParameters query)
+    {
+        if (query.Page <= 0 || query.Limit <= 0)
+        {
+            return BadRequest("Error: invalid page and limit parameters!");
+        }
+        var (users, count) = repository.GetAll(query);
+        return Ok(new { users, count });
+    }
 
-  [HttpPost("register"), AllowAnonymous]
-  public async Task<IActionResult> CreateAccount([FromBody] AccountInfo info)
-  {
-    var user = await repository.CreateAccount(info);
-    return CreatedAtAction(nameof(CreateAccount), new { user.ID }, user);
-  }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await repository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(mapper.Map<UserView>(user));
+    }
 
-  [HttpGet]
-  public IActionResult GetUsers([FromQuery] UserParameters query)
-  {
-    var (users, count) = repository.GetAll(query);
-    return Ok(new { users, count });
-  }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateRequest user)
+    {
+        await repository.UpdateAsync(id, user);
+        return Ok("User successfully updated!");
+    }
 
-  [HttpGet("{id}")]
-  public async Task<IActionResult> GetUser(int id)
-  {
-    var user = await repository.GetByIdAsync(id);
-    return Ok(user);
-  }
-
-  [HttpPut("{id}")]
-  public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateRequest user)
-  {
-    await repository.UpdateAsync(id, user);
-    return Ok("User successfully updated!");
-  }
-
-  [HttpDelete("{id}")]
-  public async Task<IActionResult> DeleteUser(int id)
-  {
-    await repository.DeleteAsync(id);
-    return Ok("User successfully deleted!");
-  }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        await repository.DeleteAsync(id);
+        return Ok("User successfully deleted!");
+    }
 }
