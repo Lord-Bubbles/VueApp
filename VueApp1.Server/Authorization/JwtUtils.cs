95af2d5373cc5c1ce36bb314dc61f3de16d78598
Lord-Bubbles <justinhanyueli@gmail.com>
2024-06-02 23:58:48 -0500

    add more claims, new methods, rename existing methods

--- VueApp1.Server/Authorization/JwtUtils.cs
+++ VueApp1.Server/Authorization/JwtUtils.cs
@@ -1,29 +1,34 @@
-using VueApp1.Server.Models;
+using VueApp1.Server.Models.Entities;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Cryptography;
-using System.Security.Claims;
-
 
 namespace VueApp1.Server.Authorization;
 
 public interface IJwtUtils
 {
-  public string GenerateToken(User user);
-  public int ValidateToken(string token);
+  public string GenerateAccessToken(User user);
+  public int ValidateAccessToken(string token);
+  public string GenerateRefreshToken();
 }
 
 public class JwtUtils : IJwtUtils
 {
-  private static readonly byte[] secret = RandomNumberGenerator.GetBytes(16);
+  public static readonly byte[] secret = RandomNumberGenerator.GetBytes(64);
 
-  public string GenerateToken(User user)
+  public string GenerateAccessToken(User user)
   {
     var tokenHandler = new JwtSecurityTokenHandler();
+    var claims = new Dictionary<string, object> {
+      {"id", Convert.ToString(user.ID)},
+      {JwtRegisteredClaimNames.Name, user.FirstName},
+      {JwtRegisteredClaimNames.FamilyName, user.LastName},
+      {JwtRegisteredClaimNames.Email, user.Email}
+    };
     var tokenDescriptor = new SecurityTokenDescriptor
     {
-      Subject = new ClaimsIdentity([new Claim("id", user.ID.ToString())]),
-      Expires = DateTime.UtcNow.AddDays(1),
+      Claims = claims,
+      Expires = DateTime.UtcNow.AddHours(1),
       SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha512Signature),
       IssuedAt = DateTime.UtcNow
     };
@@ -31,7 +36,7 @@ public class JwtUtils : IJwtUtils
     return tokenHandler.WriteToken(token);
   }
 
-  public int ValidateToken(string token)
+  public int ValidateAccessToken(string token)
   {
     if (token == null)
     {
@@ -59,5 +64,10 @@ public class JwtUtils : IJwtUtils
     {
       return -1;
     }
+  }
+
+  public string GenerateRefreshToken()
+  {
+    return Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
   }
 }
