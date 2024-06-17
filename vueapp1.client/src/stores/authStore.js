123647908356af6a01571af14b5f8d7eed923e60
Lord-Bubbles <justinhanyueli@gmail.com>
2024-06-16 23:44:16 -0500

    Modify authstore to handle login, signup, getting new access and refresh token, logout with global state for persistency

--- vueapp1.client/src/stores/authStore.js
+++ vueapp1.client/src/stores/authStore.js
@@ -1,18 +1,74 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import router from '@/router/index';
 
-export const useAuthStore = defineStore('authStore', () => {
-  const userProfile = ref(JSON.parse(localStorage.getItem('user')));
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref(JSON.parse(localStorage.getItem('user')));
 
-  function updateUser(user) {
-    userProfile.value = user;
-    localStorage.setItem('user', JSON.stringify(user));
+  async function login(form) {
+    const response = await fetch('/api/Auth/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(form)
+    });
+    if (!response.ok) {
+      alert('Incorrect username or password!');
+      return;
+    }
+    const data = await response.json();
+    user.value = data.user;
+    localStorage.setItem('token', data.accessToken);
+    localStorage.setItem('user', JSON.stringify(data.user));
+    await router.push({ name: 'home', params: { id: data.user.id } });
   }
 
-  function removeUser() {
-    userProfile.value = null;
+  async function logout() {
+    await fetch('/api/Auth/logout', {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    localStorage.removeItem('token');
     localStorage.removeItem('user');
+    user.value = null;
+    await router.push({ name: 'login' });
   }
 
-  return { userProfile, updateUser, removeUser };
+  async function refresh() {
+    const response = await fetch('/api/Auth/refresh', {
+      method: 'POST'
+    });
+    if (!response.ok) {
+      console.log('Error: invalid refresh token!');
+      user.value = null;
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      await router.push({ name: 'login' });
+      return;
+    }
+    const data = await response.json();
+    localStorage.setItem('token', data.accessToken);
+  }
+
+  async function register(form) {
+    const response = await fetch('/api/Auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(form)
+    });
+    if (!response.ok) {
+      alert('This user already exists. Please login instead!');
+      return;
+    }
+    await router.push({ name: 'login' });
+  }
+
+  return { user, login, logout, refresh, register };
 });
