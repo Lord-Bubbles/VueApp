6621b0dbd8b3ddb36e2cf5bf1211a01869dd2b76
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-02 03:17:05 -0500

    Add HomePage component

---
+++ vueapp1.client/src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import SideBar from '@/components/SideBar.vue';
+</script>
+
+<template>
+  <div class="d-flex">
+    <SideBar />
+    <div class="p-4 m-4 w-75">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+
