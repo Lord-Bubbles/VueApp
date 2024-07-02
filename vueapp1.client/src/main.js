219e20c54824f72b0141b08a378e2227c382e4f2
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-02 03:27:19 -0500

    Add toast and tanstack query plugins

--- vueapp1.client/src/main.js
+++ vueapp1.client/src/main.js
@@ -1,19 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import 'vue-toastification/dist/index.css';
 import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-//import { VueQueryPlugin } from 'vue-query';
 import App from './App.vue';
 import router from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import Toast from 'vue-toastification';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
-//app.use(VueQueryPlugin);
+app.use(VueQueryPlugin);
+app.use(Toast, {
+  position: 'top-center',
+  timeout: 2000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  closeButton: 'button',
+  icon: true,
+  rtl: false,
+  transition: 'Vue-Toastification__fade'
+});
 
 app.mount('#app');
