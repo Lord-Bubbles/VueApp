23f91db80b933ae67400c5927b80d226e1266222
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-02 03:28:55 -0500

    Completely remove all component logic since it's no longer needed

--- vueapp1.client/src/App.vue
+++ vueapp1.client/src/App.vue
@@ -1,39 +1,8 @@
 <script setup>
-  import { useRoute } from 'vue-router';
-  import { useAuthStore } from './stores/authStore';
-  import { useManagerStore } from './stores/managerStore';
-  import { onMounted, provide } from 'vue';
-
-  const store = useAuthStore();
-  const { updateUser, removeUser } = store;
-  const route = useRoute();
-  provide('user', { user: store.userProfile, updateUser, removeUser });
-
-  const managerStore = useManagerStore();
-  const { getManagers } = managerStore;
-
-  onMounted(async () => await getManagers());
-
-  provide('allManagers', { managerNames: managerStore.managerNames, getManagers });
+  import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 </script>
 
 <template>
-  <div v-if="!store.userProfile" :class="{ 'center-div': !store.userProfile }">
-    <router-view v-if="route.name === 'login'" />
-    <router-view v-else />
-  </div>
-  <div v-else>
-    <router-view />
-  </div>
+  <router-view />
+  <VueQueryDevtools />
 </template>
-
-<style scoped>
-  .center-div {
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    padding-top: 15%;
-    padding-bottom: 15%;
-    margin: 0 auto;
-  }
-</style>
