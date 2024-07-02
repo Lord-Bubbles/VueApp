24e75bd099576e1e3e56a68c38ad8dec90623720
Lord-Bubbles <justinhanyueli@gmail.com>
2024-07-02 03:10:30 -0500

    Add new service to handle api calls for performance reviews

---
+++ vueapp1.client/src/utils/performanceService.js
@@ -0,0 +1,25 @@
+import { useToast } from 'vue-toastification';
+import { fetchIntercept } from './fetchIntercept';
+
+const toast = useToast();
+
+export async function getPerformances(query) {
+  const { response, data } = fetchIntercept(
+    `/api/Performance?user=${query.user}&type=${query.type}&page=${query.page}&limit=${query.limit}`
+  );
+  if (!response.ok) {
+    throw new Error();
+  }
+  return data;
+}
+
+export async function createPerformance(formData) {
+  const { response } = fetchIntercept('/api/Performance', {
+    method: 'POST',
+    body: JSON.stringify(formData)
+  });
+  if (!response.ok) {
+    throw new Error();
+  }
+  toast.success('Performance created successfully');
+}
